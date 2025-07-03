3d5506a4e9746e2ddd008c4c1a274a07e623e884
Khan, Abdullah <abdullah.khan@ideascale.com>
2025-07-03 13:40:35 +0600

    moved to inner layer

--- build/index.js
+++
@@ -1,9 +0,0 @@
-import { mcpServer } from "./mcp-server.js";
-import { createSSEServer } from "./sse-server.js";
-import dotenv from 'dotenv';
-dotenv.config();
-const sseServer = createSSEServer(mcpServer);
-console.log("PORT", process.env.PORT);
-console.log("WORKSPACE_URL", process.env.WORKSPACE_URL);
-console.log("API_TOKEN", process.env.API_TOKEN);
-sseServer.listen(process.env.PORT || 3001);
